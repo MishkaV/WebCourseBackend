1142e5a966de73a2a91f25c85096fe310c11f8a4
Михаил Ворожцов <MAVorozhtsov@sberbank.ru>
2022-08-28 23:38:58 +0300

    add get and post response for notes

--- src/main/kotlin/io/mishka/notes/controller/NotesController.kt
+++ src/main/kotlin/io/mishka/notes/controller/NotesController.kt
@@ -1,13 +1,25 @@
 package io.mishka.notes.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import io.mishka.notes.entities.Note
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class NotesController {
 
+    private val notes: MutableList<Note> = mutableListOf()
+
     @GetMapping("/about")
     fun about(): String {
         return "COOL NOTES ABOUT"
+    }
+
+    @GetMapping("/notes")
+    fun getAllNotes(): List<Note> {
+        return notes
+    }
+
+    @PostMapping("/new-note")
+    fun setNewNote(@RequestBody note: Note) {
+        notes.add(note)
     }
 }
---
+++ src/main/kotlin/io/mishka/notes/entities/Note.kt
@@ -0,0 +1,8 @@
+package io.mishka.notes.entities
+
+
+data class Note(
+    val timespamp: String,
+    val title: String,
+    val content: String
+)
