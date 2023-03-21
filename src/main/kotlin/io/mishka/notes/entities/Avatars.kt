e429aa4fc7899ef42dd8147a8b25784a0da92e1e
MishkaV <mavorozhtsov_1@edu.hse.ru>
2023-03-21 19:10:24 +0200

    Add avatars

---
+++ src/main/kotlin/io/mishka/notes/entities/Avatars.kt
@@ -0,0 +1,35 @@
+package io.mishka.notes.entities
+
+
+val userAvatarUrls = listOf(
+        "https://startuphighway.com/wp-content/uploads/2014/12/5.jpg",
+        "https://i1.sndcdn.com/avatars-000124452487-vztyfx-t500x500.jpg",
+        "https://demo.gecdesigns.com/demo/webs/multipurpose-one-page-website-template/img/team/team-01.jpg",
+        "https://essemge.com/wp-content/uploads/2020/01/Torus-stud-earrings-mini-shiny-worn-500x500.jpg",
+        "https://yumorov.ru/wp-content/uploads/2019/10/86c19d4bced177fabca4762b282d5186.jpg",
+        "https://i1.sndcdn.com/artworks-000013566881-jurpcc-t500x500.jpg",
+        "https://i1.sndcdn.com/artworks-000201549521-hbm0qv-t500x500.jpg",
+        "https://demo.datingscript.com/uploads/8/4/2/9/B4gkm409uVAJijXanlmN_p.jpg",
+        "https://i1.sndcdn.com/artworks-000447479151-gd1i6b-t500x500.jpg",
+        "https://i.pinimg.com/736x/4b/d3/04/4bd304c14c7e47b601d0482b235a89d2--hemsworth-brothers-chris-hemsworth.jpg",
+        "https://static.tildacdn.com/tild3165-3537-4938-b066-376236306436/noroot.png",
+        "https://i1.sndcdn.com/artworks-QfSRI9OzcyB4vXmF-xBuPVA-t500x500.jpg",
+        "https://i1.sndcdn.com/avatars-000173594049-xh7yqx-t500x500.jpg",
+        "https://c1.35photo.pro/photos_temp/sizes/333/1666481_500n.jpg",
+        "https://efcreativestudios.com/wp-content/uploads/2016/06/Angelina-Jolie-Makeup-1-e1466339910825.jpg",
+        "https://ae01.alicdn.com/kf/HTB1MPMpXAvoK1RjSZFNq6AxMVXay/Bluelans-Women-Fashion-Elegant-Shiny-Waterdrop-Rhinestone-Claw-Dangle-Earrings-Jewelry.jpg",
+        "https://sun6-21.userapi.com/s/v1/ig2/OMWa6dhUD16ez2Ot0SWIcdlUzMRTaMO9hURBKKFI8Dc3aHQFO44nBg5Ukbictzn9iRqWnfoRT8Me3V0jIPmNQbIi.jpg?size=500x500&quality=95&crop=580,0,500,500&ava=1",
+        "https://i1.sndcdn.com/artworks-000142724478-r3i1hi-t500x500.jpg",
+        "https://i1.sndcdn.com/avatars-000050021469-ib40wd-t500x500.jpg",
+        "https://i1.sndcdn.com/avatars-000173658606-c1hn1r-t500x500.jpg",
+        "https://www.plasticsurgeryservices.com/wp-content/uploads/2017/09/shutterstock_150195428.jpg",
+        "https://bgjenite.com/data/uploads/2013/10/belleza-500x500_c.jpg",
+        "https://news.sjthemes.com/nw/images/500x500/28381.jpg",
+        "https://thumb-p8.xhcdn.com/a/kDRzr1hMO_2CSC5N9A42IQ/000/061/900/518_1000.jpg",
+        "https://nataschka.ru/content/cache/1/TtHrcX6rPDRmlgOZSBufgkGnoO3rl9g7.jpg/973b910676cd652c47170acaf48c7891.jpg",
+        "https://www.polsov.com/upload/1/5156_0_s.jpg",
+        "https://img1.liveinternet.ru/images/attach/c/0/117/895/117895653_shutterstock_129557474.jpg",
+        "https://live.staticflickr.com/1569/26527865905_1c68a7b2dd.jpg",
+        "https://yumorov.ru/wp-content/uploads/2019/10/76461a75c23ae4387cd8ce1daba6b888.jpg",
+        "https://c1.35photo.pro/photos_temp/sizes/123/616718_500n.jpg"
+)
--- src/main/kotlin/io/mishka/notes/entities/Post.kt
+++ src/main/kotlin/io/mishka/notes/entities/Post.kt
@@ -3,20 +3,20 @@ package io.mishka.notes.entities
 
 data class Post(
         val imgUrl: String,
-        val userUrl: String,
+        var userUrl: String = "",
         val fullName: String,
         val date: String
 )
 
 val posts = listOf(
-        Post(fullName = "John Doe", date = "March 20", imgUrl = "https://wiki.mininuniver.ru/images/thumb/6/68/%D0%9A%D1%82.jpeg/800px-%D0%9A%D1%82.jpeg", userUrl = "https://cdn.forbes.ru/files/1082x725/photo_galleries/gettyimages-867650476.jpg__1617616717__36660.webp"),
-        Post(fullName = "Mary Smith", date = "March 11", imgUrl = "https://w0.peakpx.com/wallpaper/610/992/HD-wallpaper-lovely-view-rocks-beautiful-clouds-stones-splendor-green-beauty-blue-lovely-view-colors-sky-trees-lake-tree-water-mountains-jenny-lake-peaceful-nature-landscape.jpg", userUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9udEhtw7oGvr8Wrn8bZhNDrc-aE3OO7Y7g&usqp=CAU"),
-        Post(fullName = "Bob Johnson", date = "December 20", imgUrl = "https://w0.peakpx.com/wallpaper/855/627/HD-wallpaper-british-shorthair-close-up-domestic-cat-cats-gray-cat-cute-animals-british-shorthair-cat.jpg", userUrl = "https://s0.rbk.ru/v6_top_pics/media/img/1/28/755743646058281.jpeg"),
-        Post(fullName = "Emma Wilson", date = "May 30", imgUrl = "https://cdn.fishki.net/upload/post/2017/09/19/2384535/tn/9-0.jpg", userUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3wyWByqubP28prK5mdkCYDKYZ2O5-6CQZ1-ZcdrtbX8gEfYvz-CawIYxR-6yKmrNu9jk&usqp=CAU"),
-        Post(fullName = "David Lee", date = "June 12", imgUrl = "https://stihi.ru/pics/2019/11/19/2655.jpg", userUrl = "https://klike.net/uploads/posts/2020-08/1597994286_2.jpg"),
-        Post(fullName = "Alex Jones", date = "August 29", imgUrl = "https://s0.smartresize.com/wallpaper/1/832/HD-wallpaper-corgi-bokeh-pets-welsh-corgi-dogs-lawn-welsh-corgi-dog-pembroke-welsh-corgi.jpg", userUrl = "https://wl-adme.cf.tsp.li/resize/728x/jpg/800/b64/981717537aa15c68eb1a12aaeb.jpg"),
-        Post(fullName = "Sarah Garcia", date = "March 11", imgUrl = "https://sib.fm/storage/article/March2021/eh4G8RNBUP5awP8Mxgj9.jpg", userUrl = "https://cameralabs.org/media/k2/items/cache/3cb06e4cb464be7a87ae9907c7d62b4b_L.jpg"),
-        Post(fullName = "Mike Brown", date = "September 9", imgUrl = "https://w0.peakpx.com/wallpaper/936/809/HD-wallpaper-dalmatian-dogs-lawn-domestic-dog-cute-animals-dalmatian-pets-dogs.jpg", userUrl = "https://cdn.maximonline.ru/ec/5b/70/ec5b701b6dc90d27cbde89b6e19a0d07/600x600_1_453c1a5e9b8239272a5b34d9d1f5b52b@1024x1024_0xac120002_17992516771550233711.jpg"),
-        Post(fullName = "Isabella Lopez", date = "April 1", imgUrl = "https://backoffice3.titanka.com/verticalizzazioni/4897/254/upload/1392977456_cane.jpg", userUrl = "https://st4.depositphotos.com/6903990/24055/i/600/depositphotos_240558030-stock-photo-laughing-blonde-girl-with-long.jpg"),
-        Post(fullName = "Kevin Nguyen", date = "March 18", imgUrl = "https://w0.peakpx.com/wallpaper/539/364/HD-wallpaper-autumn-fall-autumn-nature-lake-fog.jpg", userUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQckoIyOZ4Nc6IQAgwA8-rQ4uWfT1rhrpZsOw&usqp=CAU"),
-)
+        Post(fullName = "John Doe", date = "March 20", imgUrl = "https://wiki.mininuniver.ru/images/thumb/6/68/%D0%9A%D1%82.jpeg/800px-%D0%9A%D1%82.jpeg"),
+        Post(fullName = "Mary Smith", date = "March 11", imgUrl = "https://w0.peakpx.com/wallpaper/610/992/HD-wallpaper-lovely-view-rocks-beautiful-clouds-stones-splendor-green-beauty-blue-lovely-view-colors-sky-trees-lake-tree-water-mountains-jenny-lake-peaceful-nature-landscape.jpg"),
+        Post(fullName = "Bob Johnson", date = "December 20", imgUrl = "https://w0.peakpx.com/wallpaper/855/627/HD-wallpaper-british-shorthair-close-up-domestic-cat-cats-gray-cat-cute-animals-british-shorthair-cat.jpg"),
+        Post(fullName = "Emma Wilson", date = "May 30", imgUrl = "https://cdn.fishki.net/upload/post/2017/09/19/2384535/tn/9-0.jpg"),
+        Post(fullName = "David Lee", date = "June 12", imgUrl = "https://stihi.ru/pics/2019/11/19/2655.jpg"),
+        Post(fullName = "Alex Jones", date = "August 29", imgUrl = "https://s0.smartresize.com/wallpaper/1/832/HD-wallpaper-corgi-bokeh-pets-welsh-corgi-dogs-lawn-welsh-corgi-dog-pembroke-welsh-corgi.jpg"),
+        Post(fullName = "Sarah Garcia", date = "March 11", imgUrl = "https://sib.fm/storage/article/March2021/eh4G8RNBUP5awP8Mxgj9.jpg"),
+        Post(fullName = "Mike Brown", date = "September 9", imgUrl = "https://w0.peakpx.com/wallpaper/936/809/HD-wallpaper-dalmatian-dogs-lawn-domestic-dog-cute-animals-dalmatian-pets-dogs.jpg"),
+        Post(fullName = "Isabella Lopez", date = "April 1", imgUrl = "https://backoffice3.titanka.com/verticalizzazioni/4897/254/upload/1392977456_cane.jpg"),
+        Post(fullName = "Kevin Nguyen", date = "March 18", imgUrl = "https://w0.peakpx.com/wallpaper/539/364/HD-wallpaper-autumn-fall-autumn-nature-lake-fog.jpg"),
+).apply { forEachIndexed { index, post -> post.userUrl = userAvatarUrls[index] } }
